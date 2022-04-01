557d0c17038f7c6dcf5064b5e02eab8025933a99
strefilo <sergey.trefilov@telekom.com>
2022-04-01 16:39:38 +0300

    add Test

--- src/test/java/com/service/adapter/CalculatorTest.java
+++ src/test/java/com/service/adapter/CalculatorTest.java
@@ -1,38 +1,58 @@
 package com.service.adapter;
 
 import com.service.adapter.service.CalculatorService;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testng.Assert;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest()
 public class CalculatorTest {
-
     @Autowired
-    private CalculatorService calculatorService;
-/*
-    @Test
-    public void addTest(){
-        Assert.assertTrue(calculatorService.add(2,5) == 7);
+    CalculatorService calculatorService;
+
+    CalculatorService salsaOrderDetailsResult;
+
+    @Before
+    public void setup() throws Exception {
+        salsaOrderDetailsResult = mock(CalculatorService.class);
+        when(salsaOrderDetailsResult.getResoult4add(anyString())).thenReturn(Integer.valueOf(7));
+        when(salsaOrderDetailsResult.getResoult4divide(anyString())).thenReturn(Integer.valueOf(2));
+        when(salsaOrderDetailsResult.getResoult4multiply(anyString())).thenReturn(Integer.valueOf(8));
+        when(salsaOrderDetailsResult.getResoult4subtract(anyString())).thenReturn(Integer.valueOf(6));
     }
 
     @Test
-    public void divideTest(){
-        Assert.assertTrue(calculatorService.divide(4,2) == 2);
+    public void addTest() {
+        calculatorService.add(2, 5, anyString());
+        Assert.assertTrue(salsaOrderDetailsResult.getResoult4add("12342") == 7);
     }
 
     @Test
-    public void multiplyTest(){
-        Assert.assertTrue(calculatorService.multiply(4,2) == 8);
+    public void divideTest() {
+        calculatorService.divide(4, 2, "12342");
+        Assert.assertTrue(salsaOrderDetailsResult.getResoult4divide("") == 2);
     }
 
     @Test
-    public void subtractTest(){
-        Assert.assertTrue(calculatorService.subtract(8,2) == 6);
+    public void multiplyTest() {
+        calculatorService.multiply(4, 2, "1234");
+        Assert.assertTrue(salsaOrderDetailsResult.getResoult4multiply("1234") == 8);
     }
-*/
+
+    @Test
+    public void subtractTest() {
+        calculatorService.subtract(8, 2, "4321");
+        Assert.assertTrue(salsaOrderDetailsResult.getResoult4subtract("4321") == 6);
+
+    }
+
 }
